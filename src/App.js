1d05e991fcc1ad21ad73cbebbf53a2c1b77d2eea
Chiranjibzlt <38485945+Chiranjibzlt@users.noreply.github.com>
2020-08-08 22:12:14 +0530

    Updated for prod

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
-function App() {
+import { Switch,Route, Redirect } from "react-router-dom"
+import Home from "./components/Home"
+const App = () =>{
   return (
     <>
     <div>
-       <h1>Home sweet Home!!!!</h1> 
+        <>
+          <Switch> 
+              <Route Path="/" component={Home}/>
+              <Redirect to="/" />
+          </Switch>
+        </>
     </div>
     </>
   );
