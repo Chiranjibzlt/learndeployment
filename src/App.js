54f93e0d76b395d78b872224e7529310a607b298
Chiranjibzlt <38485945+Chiranjibzlt@users.noreply.github.com>
2020-08-08 23:06:56 +0530

    Build error fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch,Route, Redirect } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
