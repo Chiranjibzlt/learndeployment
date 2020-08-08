5e55075a755d310db6c0074abbc6320f39356d9b
Chiranjibzlt <38485945+Chiranjibzlt@users.noreply.github.com>
2020-08-08 11:16:32 +0530

    Updated home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Switch,Redirect } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      
+        Home sweet Home!!!!
     </div>
   );
 }
