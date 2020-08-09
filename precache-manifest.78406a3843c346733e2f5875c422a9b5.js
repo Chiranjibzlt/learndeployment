07d72a93c38acc19a8195818deb77ab9b4b5394d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-09 04:53:41 +0000

    Deploy Chiranjibzlt/learndeployment to github.com/Chiranjibzlt/learndeployment.git:gh-pages

---
+++ precache-manifest.78406a3843c346733e2f5875c422a9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4f1e68695961a2fbaa47ab478e3280",
+    "url": "/learndeployment/index.html"
+  },
+  {
+    "revision": "e75d52afd2b065e1218b",
+    "url": "/learndeployment/static/css/main.0600f202.chunk.css"
+  },
+  {
+    "revision": "13a552ea4db41dd2d64d",
+    "url": "/learndeployment/static/js/2.770298e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learndeployment/static/js/2.770298e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75d52afd2b065e1218b",
+    "url": "/learndeployment/static/js/main.9879fc7e.chunk.js"
+  },
+  {
+    "revision": "2d81d3cd97fff590cb70",
+    "url": "/learndeployment/static/js/runtime-main.ca54aded.js"
+  }
+]);
